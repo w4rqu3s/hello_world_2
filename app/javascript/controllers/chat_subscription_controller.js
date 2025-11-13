f864aa896166f657ac50b054a32c7cf2a123015f
Hanae Rosa <hanae.rosa09@gmail.com>
2025-11-13 08:43:39 -0300

    Conexão com o action cable

--- app/javascript/controllers/chat_subscription_controller.js
+++ app/javascript/controllers/chat_subscription_controller.js
@@ -1,7 +1,21 @@
 import { Controller } from "@hotwired/stimulus"
+import { createConsumer } from "@rails/actioncable"
 
-// Connects to data-controller="chat-subscription"
 export default class extends Controller {
+  static values = { chatId: Number }
+  static targets = ["messages"]
+
   connect() {
+    this.channel = createConsumer().subscriptions.create(
+      { channel: "ChatChannel", id: this.chatIdValue },
+      {
+        received: data => {
+          this.messagesTarget.insertAdjacentHTML("beforeend", data.message);
+          this.messagesTarget.scrollTop = this.messagesTarget.scrollHeight;
+          document.getElementById("message_body").value = "";
+        }
+      }
+    )
+    console.log(`Subscribed to chat ${this.chatIdValue}`);
   }
 }
